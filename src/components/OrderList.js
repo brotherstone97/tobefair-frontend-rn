c200bd38769644d6da487bad098272aac1fc9221
KimKeonAh <keon1031@naver.com>
2021-09-19 12:25:09 +0900

    주문내역 글자 정렬

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -30,7 +30,7 @@ const OrderText = styled.Text`
   font-size: 19px;
   padding-top: 8%;
   padding-bottom: 2%;
-  text-align: center;
+  margin-left: 5%;
 `; //이거 스크롤해도 보이게 할지
 const OrderContainer = styled.TouchableOpacity`
   background-color: white;
