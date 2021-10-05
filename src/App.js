530d033b82ed4e965442f9aca2367bde3a7c9906
OHS9711 <ohs9711@gmail.com>
2021-10-05 23:45:06 +0900

    App.js 필요없는 내용 삭제

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import styled from 'styled-components/native';
 import {NavigationContainer} from "@react-navigation/native";
 import StackNavigation from "./navigations/Stack";
 import {APIProvider} from "./contexts";
-import {useLoading} from "./splashScreen/useLoading";
 
-import * as SplashScreen from "expo-splash-screen";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import {fetchPost} from "./login/postSignIn";
 
 const Container = styled.View`
   flex: 1;
